85e3260df335b18cf274a5b9bb3b5b4487f20535
Tiago Crais <tiagocrais@gmail.com>
2020-05-10 21:16:03 -0300

    Criado a classe Negociacoes, afim de retornar um array de todas negociacoes, dentro de models.

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -1,12 +1,13 @@
 class NegociacaoController {
     constructor() {
+        this._negociacoes = new Negociacoes;
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
     }
     adiciona(event) {
         event.preventDefault();
-        const negociacao = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
-        console.log(negociacao);
+        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        this._negociacoes.adiciona(negociacao);
     }
 }
